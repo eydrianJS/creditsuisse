6a1f9d36e9889d7c6e4f55d85aa24fb77e71af4f
eydrianJS <adrian.olszowski@wp.pl>
2018-09-04 17:33:42 +0200

    add args[0]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,8 +9,8 @@ public class Main {
 
     public static void main(String [] args)
     {
-//        String path = args[0];
-        FileExecute file = new FileExecute("C:\\Dane\\creditsuisse\\src\\paths\\test.txt");
+        String path = args[0];
+        FileExecute file = new FileExecute(path);
         ArrayList<DatabaseMigration> ArrayResult = file.CreateJsonToObject();
         findAllPair(ArrayResult);
     }
